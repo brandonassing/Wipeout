ba5dd5e414c65d455dbd8728eba02fe8f34eeb54
Brandon Assing <assing.brandon@gmail.com>
2018-11-24 13:03:35 -0500

    something

--- tp-assist/src/App.js
+++ tp-assist/src/App.js
@@ -2,12 +2,97 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserList from './components/UserList';
+import AddForm from './components/AddForm';
+import Header from './components/Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      userList: []
+    };
+    this.remove = this.remove.bind(this);
+    this.add = this.add.bind(this);
+  }
+
+  add(key, value) {
+    this.setState({ isLoaded: false }, () => {
+      fetch('https://brandonassing.lib.id/tp-me@dev/addOneNumber/?name=' + key + '&number=' + value)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              userList: result
+            }, () => {
+            console.log(this.state);
+          });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error: error
+            });
+          }
+        );
+    });
+
+  }
+
+
+  remove(key) {
+    this.setState({ isLoaded: false }, () => {
+      fetch('https://brandonassing.lib.id/tp-me@dev/removeNumbers/?key=' + key)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              userList: result
+            }, () => {
+            console.log(this.state);
+          });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error: error
+            });
+          }
+        );
+    });
+
+  }
+
+  componentDidMount() {
+    fetch("https://brandonassing.lib.id/tp-me@dev/getNumbers/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            userList: result
+          }, () => {
+          console.log(this.state);
+        });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error
+          });
+        }
+      )
+  }
+
+
   render() {
     return (
       <div>
-        <UserList />
+        <Header name="TP ME"/>
+        <AddForm addCallback={this.add} />
+        <UserList userList={this.state.userList} isLoaded={this.state.isLoaded} removeCallback={this.remove} />
       </div>
     );
   }
