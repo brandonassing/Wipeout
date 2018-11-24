1f6b10b132465bbe317ffda507eb062c992287b8
Brandon Assing <assing.brandon@gmail.com>
2018-11-24 12:59:29 -0500

    got add working and refactored GET

--- tp-assist/src/components/AddForm.js
+++ tp-assist/src/components/AddForm.js
@@ -2,10 +2,75 @@ import React, { Component } from 'react';
 import './AddForm.css';
 
 class AddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeNumber = this.handleChangeNumber.bind(this);
+    this.state = {
+      name: "",
+      number: ""
+    };
+  }
+
+  handleChangeName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleChangeNumber(event) {
+    this.setState({
+      number: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    this.props.addCallback(this.state.name, this.state.number);
+    this.setState({
+      name: "",
+      number: ""
+    });
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
+      <form className="uk-form-horizontal uk-margin-large" onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+        </label>
+        <label>
+          Number:
+          <input type="text" value={this.state.number} onChange={this.handleChangeNumber} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
 
+
+      <form className="uk-form-horizontal uk-margin-large">
+
+          <div className="uk-margin">
+              <label className="uk-form-label" htmlFor="form-horizontal-text">Name</label>
+              <div className="uk-form-controls">
+                  <input className="uk-input" id="form-horizontal-text" type="text" placeholder="Michael Scott" />
+              </div>
+          </div>
+
+          <div className="uk-margin">
+              <label className="uk-form-label" htmlFor="form-horizontal-text">Number</label>
+              <div className="uk-form-controls">
+                  <input className="uk-input" id="form-horizontal-text" type="text" placeholder="(647) 777-7777" />
+              </div>
+          </div>
+
+          <div className="uk-margin">
+              <button className="uk-button uk-button-default">Add</button>
+          </div>
+
+      </form>
       </div>
     );
   }
--- tp-assist/src/components/UserList.js
+++ tp-assist/src/components/UserList.js
@@ -5,62 +5,20 @@ import './UserList.css';
 class UserList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isLoaded: false,
-      userList: []
-    };
     this.remove = this.remove.bind(this);
   }
 
   remove(e) {
     let key = e.target.id;
-    fetch('https://brandonassing.lib.id/tp-me@dev/removeNumbers/?key=' + key)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            userList: result
-          }, () => {
-          console.log(this.state);
-        });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error: error
-          });
-        }
-      );
+    this.props.removeCallback(key);
   }
 
-  componentDidMount() {
-    fetch("https://brandonassing.lib.id/tp-me@dev/getNumbers/")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            userList: result
-          }, () => {
-          console.log(this.state);
-        });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error: error
-          });
-        }
-      )
-  }
   render() {
-    const { error, isLoaded, userList } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
+    const { isLoaded, userList } = this.props;
+    if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
+      //TODO format number
       return (
         <table className="uk-table uk-table-striped">
             <thead>
@@ -75,7 +33,7 @@ class UserList extends Component {
                   <tr key={item[0]}>
                     <td>{item[0]}</td>
                     <td>{item[1]}</td>
-                    <td><button id={item[0]} className="uk-button uk-button-default" type="button" onClick={(e) => { this.setState({ isLoaded: false }, this.remove(e)); }}>Delete</button></td>
+                    <td><button id={item[0]} className="uk-button uk-button-default" type="button" onClick={(e) => { this.remove(e); }}>Delete</button></td>
                   </tr>
                 ))}
             </tbody>
