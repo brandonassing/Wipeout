33c68d6ed42f22a767da17779ecb48887b89b35a
jcopela7 <joncope8@gmail.com>
2018-11-23 23:56:56 -0500

    Working toilet paper navigation

--- test.py
+++ test.py
@@ -5,6 +5,12 @@ from selenium import webdriver
 driver=webdriver.Chrome()
 
 #open the website
-driver.get("https://www.google.com/")
+driver.get("https://www.amazon.ca/")
+
+id_box=driver.find_element_by_name('field-keywords')
+id_box.send_keys('toilet paper')
+id_box.submit()
+#search=driver.find_element_by_class_name('nav-search-submit-text')
+#search_button.click()
 
 driver.quit
